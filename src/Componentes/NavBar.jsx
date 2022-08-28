3fc5a808cea42d2d99a3d49983326f8558f14ef1
Facundo Pohle <facundopohle@gmail.com>
2022-08-28 18:46:59 -0300

    actualizacion

---
+++ src/Componentes/NavBar.jsx
@@ -0,0 +1,51 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../imagenes/logo.png'
+
+function Encabezado() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home" className="py-4">
+        <img
+              alt="logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+          POHLE-ART
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Stikers</Nav.Link>
+            <Nav.Link href="#pricing">Drawings</Nav.Link>
+            <NavDropdown title="Categories" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Ambient</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Caracthers
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Scenes</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Go to my telegram
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets" className="link-danger">Carrito</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Preguntas
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Encabezado;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Encabezado from './App';
+import Encabezado from './Componentes/NavBar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
