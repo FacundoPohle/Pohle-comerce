25f7d4497a63832bc17277677b42c927c3e16227
Facundo Pohle <facundopohle@gmail.com>
2022-09-28 16:37:27 -0300

    firestore

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,9 +1,10 @@
 import ItemList from './itemList';
-import customFetch from '../Utils/promesa';
-import dataFromBD from '../Utils/productos';
+// import customFetch from '../Utils/promesa';
+// import dataFromBD from '../Utils/productos';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import { db } from '../Utils/firebaseConfig';
+import { db } from '../Utils/firebaseConfig';
+import { query, orderBy, where, collection, getDocs } from "firebase/firestore";
 
 
 function ItemListContainer() {
@@ -11,23 +12,44 @@ function ItemListContainer() {
   const { idCategory } = useParams();
 
   useEffect(() => {
-
-    if(idCategory){
-      customFetch(2000, dataFromBD.filter(item => item.categoryId === parseInt(idCategory)))
-        .then(result => setData(result))
-        .catch(err => console.log(err))
-    } else {
-      customFetch(2000, dataFromBD)
-      .then(result => setData(result))
-      .catch(err => console.log(err))
-    }
-      
+    // if(idCategory){
+    //   customFetch(2000, dataFromBD.filter(item => item.categoryId === parseInt(idCategory)))
+    //     .then(result => setData(result))
+    //     .catch(err => console.log(err))
+    // } else {
+    //   customFetch(2000, dataFromBD)
+    //   .then(result => setData(result))
+    //   .catch(err => console.log(err))
+    // }   
+    async function fetchData() {
+      let item;
+      if (idCategory) {
+        item = query(collection(db, "products"), where('categoryId', '==', idCategory));
+      } else {
+        item = query(collection(db, "products"), orderBy('name'));
+      }
+      const querySnapshot = await getDocs(item);
+  const dataFromFirestore = querySnapshot.docs.map(item => ({
+    id: item.id,
+    ...item.data()
+  }))
+  setData(dataFromFirestore)
+}
+fetchData();
   }, [idCategory]);
-  return (
-    <div className="row justify-content-around">
-      <ItemList items={ data } />
-    </div>
-  );
+
+// useEffect(() => {
+//   return (() => {
+//     setData([]);
+//   })
+// }, [])
+
+
+return (
+  <div className="row justify-content-around">
+    <ItemList items={data} />
+  </div>
+);
 }
 
 export default ItemListContainer;
--- src/Components/itemDetailContainer.jsx
+++ src/Components/itemDetailContainer.jsx
@@ -1,23 +1,46 @@
 import { useEffect, useState } from "react";
-import customFetch from "../Utils/promesa";
+// import customFetch from "../Utils/promesa";
 import ItemDetail from "./itemDetail";
-import dataFromBD from "../Utils/productos";
+// import dataFromBD from "../Utils/productos";
 import { useParams } from "react-router-dom";
+import { db } from '../Utils/firebaseConfig';
+import { doc, getDoc } from "firebase/firestore";
 
 
 const ItemDetailContainer = () => {
     const [dato, setDato] = useState({});
-    const {idItem} = useParams();
+    const { idItem } = useParams();
+
 
     useEffect(() => {
-        console.log(idItem, dataFromBD)
-        customFetch(2000, dataFromBD.find(item => item.id === parseInt(idItem)))
-           .then(result => setDato(result))
-           .catch(err => console.log(err))
+        async function fetchData() {
+            const docRef = doc(db, "products", idItem);
+            const docSnap = await getDoc(docRef);
+
+            if(docSnap.exists()) {
+                return {
+                    id: idItem,
+                    ...docSnap.data()
+                }
+            } else {
+                console.log('no such document')
+            }
+            setDato(docSnap)
+        }
+
+        fetchData();
     }, [idItem]);
-    
+
+
+    // useEffect(() => {
+    //     console.log(idItem, dataFromBD)
+    //     customFetch(2000, dataFromBD.find(item => item.id === parseInt(idItem)))
+    //        .then(result => setDato(result))
+    //        .catch(err => console.log(err))
+    // }, [idItem]);
+
     return (
-        <ItemDetail item={dato}/>
+        <ItemDetail item={dato} />
     );
 }
 
